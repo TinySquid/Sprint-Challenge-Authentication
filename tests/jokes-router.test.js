e6393c2836e1d6437529fe3ce456265bc25e24ac
Michael Nunes <miketnunes@gmail.com>
2019-12-20 09:14:50 -0800

    Add tests for jokes endpoint

--- tests/jokes-router.test.js
+++ tests/jokes-router.test.js
@@ -1,17 +1,85 @@
 const request = require("supertest");
 
+const db = require("../database/dbConfig");
+
 const server = require("../api/server");
 
 //* JOKES ROUTER
 describe("Jokes-Router", () => {
+  //? Clear database before each test
+  beforeEach(async () => {
+    await db("users").truncate();
+  });
+
   test("returns 200 status code for auth users", () => {
-    //TODO add test
+    const newUser = { username: "RedMoon15", password: "claDS*123" };
+    const existingUser = { username: "RedMoon15", password: "claDS*123" };
+
+    const expectedStatusCode = 200;
+
+    return request(server)
+      .post("/api/auth/register")
+      .send(newUser)
+      .then(() => {
+        return request(server)
+          .post("/api/auth/login")
+          .send(existingUser)
+          .expect(200)
+          .then((res) => {
+            const token = res.body.token;
+
+            return request(server)
+              .get("/api/jokes")
+              .set("authorization", token)
+              .expect(expectedStatusCode);
+          });
+      });
   });
   test("returns correct content-type", () => {
-    //TODO add test
+    const newUser = { username: "RedMoon15", password: "claDS*123" };
+    const existingUser = { username: "RedMoon15", password: "claDS*123" };
+
+    return request(server)
+      .post("/api/auth/register")
+      .send(newUser)
+      .then(() => {
+        return request(server)
+          .post("/api/auth/login")
+          .send(existingUser)
+          .expect(200)
+          .then((res) => {
+            const token = res.body.token;
+
+            return request(server)
+              .get("/api/jokes")
+              .set("authorization", token)
+              .expect("Content-Type", /json/);
+          });
+      });
   });
 
   test("returns array of jokes", () => {
-    //TODO add test
+    const newUser = { username: "RedMoon15", password: "claDS*123" };
+    const existingUser = { username: "RedMoon15", password: "claDS*123" };
+
+    return request(server)
+      .post("/api/auth/register")
+      .send(newUser)
+      .then(() => {
+        return request(server)
+          .post("/api/auth/login")
+          .send(existingUser)
+          .expect(200)
+          .then((res) => {
+            const token = res.body.token;
+
+            return request(server)
+              .get("/api/jokes")
+              .set("authorization", token)
+              .then((res) => {
+                expect(Array.isArray(res.body)).toBe(true);
+              });
+          });
+      });
   });
 });
